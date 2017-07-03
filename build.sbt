72a46c13d9d2891604a9c8df88225184929b2c31
Paweł Bartkiewicz <tuuresairon@gmail.com>
2017-07-03 02:56:51 +0200

    Implemented basic deposit calculator with hard-coded deposit offers.

--- build.sbt
+++ build.sbt
@@ -9,7 +9,12 @@ lazy val root = project
     )),
     name := "Binding.scala Deposit Calculator",
     libraryDependencies ++= Seq(
-      "org.scala-js" %%% "scalajs-dom" % "0.9.2"
+      "org.scala-js"             %%% "scalajs-dom" % "0.9.2",
+      "com.thoughtworks.binding" %%% "binding"     % "10.0.2",
+      "com.thoughtworks.binding" %%% "dom"         % "10.0.2"
+    ),
+    addCompilerPlugin(
+      "org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full
     ),
     scalaJSUseMainModuleInitializer := true
   )
--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,17 +1,70 @@
 import scala.scalajs.js.JSApp
 
-import org.scalajs.dom
-import dom.{ document, window }
+import org.scalajs.dom.document
+import org.scalajs.dom.raw.{ Event, HTMLInputElement }
+import com.thoughtworks.binding.{ Binding, dom }
+import com.thoughtworks.binding.Binding.{ Var, Vars }
 
 object MainApp extends JSApp {
 
-  def main(): Unit = {
-    println("Starting 'Binding.scala Deposit Calculator'...")
+  case class Deposit(period: Int, interest: Double) {
+    def gain(amount: Double, duration: Int) = {
+      val interestPerPeriod = interest * (period / 12.0)
+      val fullPeriods       = duration / period
+      val finalBalance      = amount * math.pow(1 + interestPerPeriod, fullPeriods)
 
-    val p = document.createElement("p")
-    val text = document.createTextNode("Hello!")
-    p.appendChild(text)
-    document.body.appendChild(p)
+      finalBalance - amount
+    }
+  }
+
+  val amount   : Var[Double]   = Var(10000.0)
+  val duration : Var[Int]      = Var(12)
+  val deposits : Vars[Deposit] = Vars(Deposit(1, 0.02), Deposit(3, 0.025), Deposit(12, 0.03))
+
+  @dom def renderMainContainer(amount: Var[Double], duration: Var[Int], deposits: Vars[Deposit]) = {
+    <div>
+      { renderAmountInput(amount).bind }
+      { renderDurationInput(duration).bind }
+      { renderDepositsTable(amount, duration, deposits).bind }
+    </div>
+  }
+
+  @dom def renderAmountInput(amount: Var[Double]) = {
+    <input type="number" id="amountInput"
+      value={ amount.bind.toString }
+      onchange={ (e: Event) => amount := amountInput.value.toDouble }
+      />
+  }
+
+  @dom def renderDurationInput(duration: Var[Int]) = {
+    <input type="number" id="durationInput"
+      value={ duration.bind.toString }
+      onchange={ (e: Event) => duration := durationInput.value.toInt }
+      />
+  }
+
+  @dom def renderDepositsTable(amount: Binding[Double], duration: Binding[Int], deposits: Vars[Deposit]) = {
+    <table>
+      { deposits.map(renderDeposit(_, amount, duration).bind) }
+    </table>
+  }
+
+  @dom def renderDeposit(deposit: Deposit, amount: Binding[Double], duration: Binding[Int]) = {
+    <tr>
+      <td>{ deposit.period.toString }</td>
+      <td>{ (deposit.interest * 100).toString }%</td>
+      <td>
+        {
+          val gain = deposit.gain(amount.bind, duration.bind)
+          f"${gain}%.2f"
+        }
+      </td>
+    </tr>
+  }
+
+  def main(): Unit = {
+    dom.render(document.getElementById("mainContainer"),
+               renderMainContainer(amount, duration, deposits))
   }
 
 }
